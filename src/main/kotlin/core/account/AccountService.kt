1032215d97828762ad3e30e18ead7195a4a9b90b
matsurihime <pipikapu@gmail.com>
2017-10-18 16:00:09 +0900

    update kdoc

--- src/main/kotlin/core/account/AccountService.kt
+++ src/main/kotlin/core/account/AccountService.kt
@@ -8,28 +8,68 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
+const val ROUTE = "/1.1/account"
+
 interface AccountService{
-    @GET("/1.1/account/settings.json")
+
+    /**
+     * Get account settings by using [token].
+     * @return Settings
+     */
+    @GET("$ROUTE/settings.json")
     fun getAccountSettings(@Header("token") token: String): Observable<Response<Settings>>
 
-    @GET("/1.1/account/verify_credentials.json")
+    /**
+     * Verify Credentials by using [token].
+     * @return User data
+     */
+    @GET("$ROUTE/verify_credentials.json")
     fun verifyCredentials(@Header("token") token: String): Observable<Response<User>>
 
-    @POST("")
-    fun removeProfileBanner()
+    /**
+     * Remove user's profile banner
+     *
+     * Banner [image] must be a valid GIF, JPG, or PNG image of less than 800KB in size.
+     * Images with larger than 2048 pixels will bi forcibily scale down.
+     * The image must be provided as raw multipart data, not a URL
+     * You can use [mediaId] instead of base64 image string.
+     */
+    // UNDONE
+    @POST("$ROUTE/profile_banner.json")
+    fun removeProfileBanner(image: String?,
+                            mediaId: Long?,
+                            tile: Boolean = false,
+                            includeEntities: Boolean = false,
+                            skipStatus: Boolean = true
+                            )
 
+    /**
+     * Update user's settings.
+     */
     @POST("")
     fun updateSettings()
 
+    /**
+     * Update user's profile.
+     */
     @POST("")
     fun updateProfile()
 
-    @POST("")
+    /**
+     * Update user's profile background image.
+     */
+    @POST("$ROUTE/update_profile_background_image.json")
     fun updateProfileBackgroundImage()
 
+    /**
+     * Update user's profile banner.
+     */
     @POST("")
     fun updateProfileBanner()
 
+    /**
+     * Update user's profile image.
+     */
     @POST("")
     fun updateProfileImage()
 
