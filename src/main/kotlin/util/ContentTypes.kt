bb73fd271ca847856c4d819c1b0f2ad75e1d281d
matsurihime <pipikapu@gmail.com>
2017-10-19 13:15:24 +0900

    enum追加

---
+++ src/main/kotlin/util/ContentTypes.kt
@@ -0,0 +1,8 @@
+package util
+
+/**
+ * Created by matsurihime on 2017/10/19.
+ */
+object ContentTypes{
+    val TEXT_PLAIN : String = "text/plain"
+}
---
+++ src/main/kotlin/util/HttpMethods.kt
@@ -0,0 +1,9 @@
+package util
+
+object HttpMethods{
+    val GET = "GET"
+    val PUT = "PUT"
+    val POST = "POST"
+    val DELETE = "DELETE"
+    val PATCH = "PATCH"
+}
